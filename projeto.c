fc47d2952a3bcfbafc99db9b69a6a4780bf5fc62
Júlio Faúndes <mr.zizima@gmail.com>
2020-11-17 02:28:02 -0300

    Segundo Commit

--- projeto.c
+++ projeto.c
@@ -11,14 +11,15 @@
 typedef struct disciplina
 {
     char nomeDisciplina[15];
-    float nota1, nota2;
+    float nota1;
+    float nota2;
 } cDisciplina;
 
 typedef struct infoAluno
 {
     char nome[60];
-    int RA; //definir máximo de 99999999.
-    cDisciplina disciplina[maxDisciplinas];
+    int RA;
+    cDisciplina disciplinaAluno[maxDisciplinas];
 } cAluno;
 
 void End()
@@ -29,23 +30,106 @@ void End()
     end(0);
 }
 
-void CadastrarDisciplinas(cDisciplina cadastroDisciplinas[maxDisciplinas])
+void InserirAlunos(cAluno cadastroAlunos[maxAlunos], int *nAlunosAtual, cDisciplina cadastroDisciplinas[maxDisciplinas], int nDisciplinas)
 {
-    int nDisciplinas;
+    int nAlunosCadastrar;
 
     do
     {
         system("clear");
+        printf("Quantos alunos deseja cadastrar?\nNúmero: ");
+        scanf("%d", &nAlunosCadastrar);
+        getchar();
+
+        if(*nAlunosAtual + nAlunosCadastrar > maxAlunos)
+        {
+            printf("\n\n--------\nNúmero inválido. O valor colocado ultrapassaria o máximo de alunos registrado. . .");
+            getchar();
+        }
+    } while (*nAlunosAtual + nAlunosCadastrar > maxAlunos);
+
+
+    for(int i = 0; i < nAlunosCadastrar; i++)
+    {
+        int select;
+
+        system("clear");
+        printf("Cadastrando o aluno nº%d:\n", *nAlunosAtual + 1);
+        printf("Digite o nome completo: ");
+        gets(cadastroAlunos[*nAlunosAtual].nome);
+        printf("Digite seu RA [9 Dígito]: ");
+        scanf("%d", &cadastroAlunos[*nAlunosAtual].RA);
+        getchar();
+
+        do
+        {
+
+            system("clear");
+            printf("As disciplinas disponíveis são:\n");
+
+            for(int j = 0; j < nDisciplinas; j++)
+            {
+                printf("[ %d ] - %s\n", j, cadastroDisciplinas[j].nomeDisciplina);
+            }
+
+            printf("\nSelecione a disciplina: ");
+            scanf("%d", &select);
+            // getchar();
+
+            if(select < 0 || select >= nDisciplinas)
+            {
+                printf("\n--------\n\nPor favor, insira um valor válido. . .");
+                getchar();
+            }
+        } while (select < 0 || select >= nDisciplinas);
+
+        // Agora, a partir do select, deve ser inserido que o aluno faz a disciplina correspondente ao endereço cadastroDisciplinas[select].nomeDisciplina no endereço cadastroAlunos[*nAlunoAtual].disciplinaAluno[select]. Acho que da para fazer olhando apenas para posição, sem precisar copiar o nome. De qualquer forma, vai se preciso da uma boa experimentada em como funciona um struct dentro de outro struct.
+    }
+}
+
+void RemocaoDeAlunos()
+{
+
+}
+
+void AlteracaoDeInformacoes()
+{
+
+}
+
+void BuscarAluno()
+{
+
+}
+
+void ListarNotas()
+{
+
+}
+
+void ListaDisciplina()
+{
+
+}
+
+void CadastrarDisciplinas(cDisciplina cadastroDisciplinas[maxDisciplinas],int *nDisciplinas)
+{
+    do
+    {
+        system("clear");
         printf("Quantas disciplinas existem? [ Máximo: %d ]\nDigite: ", maxDisciplinas);
-        scanf("%d", &nDisciplinas);
+        scanf("%d", &*nDisciplinas);
         getchar();
         
-        if(nDisciplinas < 0 || nDisciplinas > 7)
-            printf("\nPor favor, insira um valor válido. . .");
-    } while (nDisciplinas < 0 || nDisciplinas > 7);
+        if(*nDisciplinas < 0 || *nDisciplinas > 7)
+        {
+            printf("\n--------\n\nPor favor, insira um valor válido. . .");
+            getchar();
+        }
+    } while (*nDisciplinas < 0 || *nDisciplinas > 7);
     
     system("clear");
-    for(int i = 0; i < nDisciplinas; i++)
+    for(int i = 0; i < *nDisciplinas; i++)
     {
         printf("\nDigite o nome da %dª disciplina: ", i + 1);
         gets(cadastroDisciplinas[i].nomeDisciplina);
@@ -54,11 +138,12 @@ void CadastrarDisciplinas(cDisciplina cadastroDisciplinas[maxDisciplinas])
 
 int main()
 {
-    int loop = 1;
+    int loop = 1, nDisciplinas, nAlunosAtual = 0;
     cAluno cadastroAlunos[maxAlunos];
     cDisciplina cadastroDisciplinas[maxDisciplinas];
 
-    CadastrarDisciplinas(cadastroDisciplinas);
+    CadastrarDisciplinas(cadastroDisciplinas, &nDisciplinas);
+
     // Menu do Programa.
     do
     {
@@ -71,10 +156,11 @@ int main()
         printf("[ 2 ] - Remover Aluno\n");
         printf("[ 3 ] - Alterar Informações\n");
         printf("[ 4 ] - Buscar Aluno\n");
-        printf("[ 5 ] - Mostrar Informações dos Alunos\n");
+        printf("[ 5 ] - Listar Notas\n");
         printf("[ 6 ] - Mostrar Alunos de uma disciplina\n");
         printf("Resposta: ");
         scanf("%d", &escolha);
+        getchar();
 
         switch (escolha)
         {
@@ -82,19 +168,31 @@ int main()
             End();
             break;
         case 1:
+            if(nAlunosAtual >= maxAlunos)
+            {
+                printf("\n\n--------\nO número máximo de alunos já foi atingido!");
+                getchar();
+            }
+            else
+                InserirAlunos(cadastroAlunos, &nAlunosAtual, cadastroDisciplinas, nDisciplinas);
             break;
         case 2:
+            RemocaoDeAlunos();
             break;
         case 3:
+            AlteracaoDeInformacoes();
             break;
         case 4:
+            BuscarAluno();
             break;
         case 5:
+            ListarNotas();
             break;
         case 6:
+            ListaDisciplina();
             break;
         }
-    } while (loop != 1);
+    } while (loop == 1);
     
     getchar();getchar();
     return 0;
